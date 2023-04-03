16f76e2e4555f52a9ad1b7b452540feb9775b8d5
Igor Konov <112283044+Bankin93@users.noreply.github.com>
2023-04-03 16:49:27 +0300

    hw_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employees_id serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
+);
+SELECT * FROM employees
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+SELECT * FROM customers
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employees_id) NOT NULL,
+	order_date date,
+	ship_city varchar(100) NOT NULL
+);
+SELECT * FROM orders
